ea6dcd0974266f64595217e712fcf0644b84bd99
sahdev50 <dxdestroyer007@gmail.com>
2024-01-06 21:40:55 +0000

    made some starter changes

---
+++ server/src/constants/index.js
@@ -0,0 +1,8 @@
+require("dotenv").config()
+
+module.exports = {
+    PORT:process.env.PORT,
+    SERVER_URL:process.env.SERVER_URL,
+    CLIENT_URL:process.env.CLIENT_URL,
+    SECRET:process.env.SECRET
+}
