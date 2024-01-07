5521cf6bb57a85f2df92f02a9f05efac7db79755
sahdev50 <dxdestroyer007@gmail.com>
2024-01-07 19:04:18 +0000

    auth server created with passport and jwt

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,36 @@
-console.log("server handler")
+const express = require("express")
+const app = express()
+const cookieParser = require("cookie-parser")
+const passport = require('passport')
+const cors = require('cors')
+
+// constants
+const {PORT, SERVER_URL, CLIENT_URL} = require("./constants")
+
+// Routes
+const authRouter = require("./routes/auth")
+
+//import passport middleware
+require('./middlewares/passport-middleware')
+
+//initialize middlewares
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({ origin: CLIENT_URL, credentials: true }))
+app.use(passport.initialize())
+
+// initialize routes
+app.use("/api", authRouter)
+
+// main app
+const appStart = () => {
+    try {
+        app.listen(PORT, ()=>{
+            console.log(`server started at... ${SERVER_URL}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+appStart()
